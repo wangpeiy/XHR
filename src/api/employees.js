2fea262c358d94726ecd20bed2d8a9bfee26d684
wangpeiyu <1227270970@qq.com>
2022-10-07 15:22:33 +0800

    feat:人员管理API封装完成

---
+++ src/api/employees.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+/**
+ * 获取员工的综合列表数据
+ * ***/
+export function getEmployeeList(params) {
+  return request({
+    url: '/sys/user',
+    params
+  })
+}
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,25 +1,66 @@
 <template>
-  <div>
-    <PageTools type="error11" :show-before="false">
-      <!-- before:作用域插槽 -->
+  <div class="app-container">
+    <PageTools>
       <template #before>
-        <span>共25条记录</span>
+        <span>共166条记录</span>
       </template>
-
-      <!-- after:作用域插槽 -->
       <template #after>
         <el-button size="small" type="warning">导入</el-button>
         <el-button size="small" type="danger">导出</el-button>
+        <el-button size="small" type="primary">新增员工</el-button>
       </template>
     </PageTools>
+    <!-- 放置表格和分页 -->
+    <el-card>
+      <el-table border>
+        <el-table-column label="序号" sortable="" width="80" />
+        <el-table-column label="姓名" />
+        <el-table-column label="工号" />
+        <el-table-column label="聘用形式" />
+        <el-table-column label="部门" />
+        <el-table-column label="入职时间" sortable="" />
+        <el-table-column label="账户状态" />
+        <el-table-column label="操作" fixed="right" width="280">
+          <template>
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">转正</el-button>
+            <el-button type="text" size="small">调岗</el-button>
+            <el-button type="text" size="small">离职</el-button>
+            <el-button type="text" size="small">角色</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <el-row type="flex" justify="end" align="middle" style="height: 60px">
+        <el-pagination layout="prev, pager, next" />
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
 // import PageTools from '@/components/PageTools/index.vue'
-
+import { getEmployeeList } from '@/api'
 export default {
   // components: { PageTools }
+  data() {
+    return {
+      page: {
+        page: 1,
+        size: 10
+      }
+    }
+  },
+  mounted() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      const res = await getEmployeeList(this.page)
+      console.log(res)
+    }
+  }
 }
 </script>
 
